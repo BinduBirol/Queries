e7aa0368eda38d538d355fd630b8c08c5b837ded
Bindu Birol <33890858+BinduBirol@users.noreply.github.com>
2018-03-14 18:35:45 +0600

    modified query

--- prottyaanpotro1.sql
+++ prottyaanpotro1.sql
@@ -1,71 +1,35 @@
-/* Formatted on 3/12/2018 11:24:44 AM (QP5 v5.227.12220.39754) */
-  SELECT CUSTOMER_ID,
-         FULL_NAME,
-         FATHER_NAME,
-         ADDRESS_LINE1,
-         PHONE,
-         SDATE,
-         TO_DATE (SYSDATE, 'dd-mm-yyyy') prattayandate,
-         NEXTDAY,
-         PYEAR,
-         SUM (OLD_APPLIANCE_QNT_BILLCALL) OLD_APPLIANCE_QNT_BILLCALL,
-         SUM (MAX_LOAD) MAX_LOAD
-    FROM (SELECT CUSTOMER_ID,
-                 FULL_NAME,
-                 FATHER_NAME,
-                 ADDRESS_LINE1,
-                 PHONE,
-                 SDATE,
-                 NEXTDAY,
-                 PYEAR,
-                 OLD_APPLIANCE_QNT_BILLCALL,
-                 MAX_LOAD
-            FROM (SELECT cp.CUSTOMER_ID,
-                         cp.FULL_NAME,
-                         cp.FATHER_NAME,
-                         cp.ADDRESS_LINE1,
-                         cp.MOBILE PHONE,
-                         TO_CHAR (SYSDATE) SDATE,
-                         TO_CHAR (SYSDATE + 20) NEXTDAY,
-                         EXTRACT (YEAR FROM SYSDATE) - 1 PYEAR,
-                         OLD_APPLIANCE_QNT_BILLCALL,
-                         NVL (MAX_LOAD, 0) MAX_LOAD
-                    FROM (SELECT OLD_APPLIANCE_QNT_BILLCALL, Customer_id
-                            FROM burner_qnt_change
-                           WHERE pid IN (SELECT MAX (PID)
-                                           FROM burner_qnt_change
-                                          WHERE customer_id = '010114230')) tab1,
-                         MVIEW_CUSTOMER_INFO cp
-                   WHERE CP.CUSTOMER_ID = TAB1.CUSTOMER_ID)
-          UNION ALL
-          SELECT CUSTOMER_ID,
-                 FULL_NAME,
-                 FATHER_NAME,
-                 ADDRESS_LINE1,
-                 PHONE,
-                 SDATE,
-                 NEXTDAY,
-                 PYEAR,
-                 NVL (NEW_DOUBLE_BURNER_QNT_BILLCAL, 0)
-                    NEW_DOUBLE_BURNER_QNT_BILLCAL,
-                 MAX_LOAD
-            FROM (SELECT CUSTOMER_ID,
-                         FULL_NAME,
-                         FATHER_NAME,
-                         ADDRESS_LINE1,
-                         MOBILE PHONE,
-                         TO_CHAR (SYSDATE) SDATE,
-                         TO_CHAR (SYSDATE + 20) NEXTDAY,
-                         EXTRACT (YEAR FROM SYSDATE) - 1 PYEAR,
-                         NULL NEW_DOUBLE_BURNER_QNT_BILLCAL,
-                         MAX_LOAD
-                    FROM MVIEW_CUSTOMER_INFO
-                   WHERE CUSTOMER_ID = '010114230'))
-GROUP BY CUSTOMER_ID,
-         FULL_NAME,
-         FATHER_NAME,
-         ADDRESS_LINE1,
-         SDATE,
-         NEXTDAY,
-         PYEAR,
-         PHONE
+SELECT * 
+  FROM (  SELECT bi.CUSTOMER_ID, 
+                 CUSTOMER_CATEGORY, 
+                 bi.AREA_ID, 
+                 LISTAGG ( 
+                       TO_CHAR (TO_DATE (BILL_MONTH, 'MM'), 'MON') 
+                    || ' ' 
+                    || SUBSTR (BILL_YEAR, 3), 
+                    ',') 
+                 WITHIN GROUP (ORDER BY BILL_YEAR ASC, BILL_MONTH ASC) 
+                    AS DUEMONTH, 
+                 SUM ( 
+                      BILLED_AMOUNT 
+                    + CALCUALTESURCHARGE (BILL_ID, 
+                                          TO_CHAR (SYSDATE, 'dd-mm-YYYY'))) 
+                    totalamount, 
+                 COUNT (*) cnt 
+            FROM BILL_METERED bi, CUSTOMER_CONNECTION cc 
+           WHERE     BI.CUSTOMER_ID = CC.CUSTOMER_ID 
+                 AND CC.STATUS = 1 
+                 AND bi.STATUS = 1 
+                 AND bi.area_id = '01' 
+                 --AND BI.CUSTOMER_ID = '010300002/25' 
+                -- And bi.CUSTOMER_CATEGORY= 
+                 AND BILL_YEAR || LPAD (BILL_MONTH, 2, 0) <= '201601'
+        GROUP BY BI.CUSTOMER_ID, CUSTOMER_CATEGORY, bi.AREA_ID 
+          HAVING COUNT (*) > 1) tmp1, 
+       (SELECT AA.CUSTOMER_ID, 
+               BB.FULL_NAME, 
+               BB.MOBILE, 
+               AA.ADDRESS_LINE1, 
+               AA.ADDRESS_LINE2 
+          FROM CUSTOMER_ADDRESS aa, CUSTOMER_PERSONAL_INFO bb 
+         WHERE AA.CUSTOMER_ID = BB.CUSTOMER_ID) tmp2 
+ WHERE tmp1.CUSTOMER_ID = tmp2.CUSTOMER_ID 
